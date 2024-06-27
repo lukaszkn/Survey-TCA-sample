62a077a99ee0b50897c644989f0e830dc8deae34
Lukasz Knap <lukaszkn@go2.pl>
2024-06-27 14:46:51 +0200

    Initial Xcode project setup

---
+++ Survey/Survey/SurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyApp.swift
+//  Survey
+//
+//  Created by Lukasz on 27/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
